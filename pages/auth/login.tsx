23ba936f577ddcf516ab2d95b598f29f3a038c60
Ko-GyeongTae <kokt0203@naver.com>
2021-12-01 23:29:42 +0900

    EDIT

--- pages/auth/login.tsx
+++ pages/auth/login.tsx
@@ -12,29 +12,42 @@ export default function Login() {
                 <link rel="icon" href="/favicon.ico" />
             </Head>
             <main>
-                <form className="ui form">
-                    <div className="field">
-                        <label>First Name</label>
-                        <input type="text" name="first-name" placeholder="First Name" />
+                <div style={{ width: 450 }}>
+                    <div className="ui middle aligned center aligned grid">
+                        <div className="column">
+                            <h2 className="ui teal image header">
+                                <img src="assets/images/logo.png" className="image" />
+                                <div className="content">
+                                    Log-in to your account
+                                </div>
+                            </h2>
+                            <form className="ui large form">
+                                <div className="ui stacked segment">
+                                    <div className="field">
+                                        <div className="ui left icon input">
+                                            <i className="user icon"></i>
+                                            <input type="text" name="email" placeholder="E-mail address" />
+                                        </div>
+                                    </div>
+                                    <div className="field">
+                                        <div className="ui left icon input">
+                                            <i className="lock icon"></i>
+                                            <input type="password" name="password" placeholder="Password" />
+                                        </div>
+                                    </div>
+                                    <div className="ui fluid large teal submit button">Login</div>
+                                </div>
+
+                                <div className="ui error message"></div>
+
+                            </form>
+
+                            <div className="ui message">
+                                New to us? <a href="#">Sign Up</a>
+                            </div>
+                        </div>
                     </div>
-                    <div className="field">
-                        <label>Last Name</label>
-                        <input type="text" name="last-name" placeholder="Last Name" />
-                    </div>
-                    <div className="field">
-                        <label>Last Name</label>
-                        <input type="text" name="last-name" placeholder="Last Name" />
-                    </div>
-                    <div className="field">
-                        <label>Last Name</label>
-                        <input type="text" name="last-name" placeholder="Last Name" />
-                    </div>
-                    <div className="field">
-                        <label>Last Name</label>
-                        <input type="text" name="last-name" placeholder="Last Name" />
-                    </div>
-                    <button className="ui button" type="submit">Submit</button>
-                </form>
+                </div>
             </main>
             <footer>
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -23,7 +23,7 @@ export default function Home() {
                 <a className="item">Company</a>
                 <a className="item">Careers</a>
                 <div className="right item">
-                  <a className="ui inverted button">Log in</a>
+                  <a href="/auth/login" className="ui inverted button">Log in</a>
                   <a className="ui inverted button">Sign Up</a>
                 </div>
               </div>
